a00b3f32cd32a5455aaa38422648962e7fb99bd0
panjuening <juening.pan@outlook.com>
2026-04-25 01:11:31 +0800

    4_25Webexeperience

--- thepart2/part2exercise2/src/App.jsx
+++ thepart2/part2exercise2/src/App.jsx
@@ -28,11 +28,12 @@ return(
 }
 
 
- const Persons = ({ filteredPersons }) => {
+ const Persons = ({ filteredPersons,toggleTrash }) => {
   return (
     <ul>
       {filteredPersons.map(person => (
-        <li key={person.name}>{person.name} {person.number}</li>
+        <li key={person.name}>{person.name} {person.number}
+        <button onClick={() =>toggleTrash(person.id)}>Go to Trash</button></li>
       ))}
     </ul>
   );
@@ -60,27 +61,62 @@ useEffect(() => {
 const addPerson=(event)=>
 {
 event.preventDefault()
-  const nameExists = persons.some(person => person.name === newName); 
-     if (nameExists) {
-      alert(`${newName} 已存在于电话簿中`);
-      return;
-      }
-
-  const PersonObject = {
+const PersonObject = {
     name: newName,
     number:newNumber,
+    id:newName+'i',
   }
+  const nameExists = persons.some(person => person.name === newName); 
+     if (nameExists) {
+       if(window.confirm(`${newName} 已存在于电话簿中,是否需要替换?`))
+       {
+        const url = `http://localhost:3001/persons/${PersonObject.id}`
+        const person = persons.find(n => n.id === PersonObject.id)
+        const changedNote = { ...person, number: newNumber }
+        axios.put(url, changedNote).then(response => {
+    setPersons(persons.map(person => person.id === PersonObject.id ? response.data : person))
+         setNewName('')
+    setNewNumber('') 
+  })
 
-  setPersons(persons.concat(PersonObject))
-  setNewName('')
-  setNewNumber('')
+       }
+       else{alter('未替换')}
+
+
+      }
+
+  
+ axios
+    .post('http://localhost:3001/persons', PersonObject)
+    .then(response => {
+      setPersons(persons.concat(response.data))
+      console.log(response.data)
+      setNewName('')
+    setNewNumber('')})
 }
 
 const filteredPersons = persons.filter(person => 
     person.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+  
 
+const toggleTrashOf = (id) => {
+  const url = `http://localhost:3001/persons/${id}`
+     if(window.confirm("Delect "+`${id}`))
+     {
+        axios.delete(url)
+  .then(response => {
+    console.log('Resource deleted successfully:',response.data)
+  })
+  .catch(error => {
+    console.error('Error deleting resource:', error)
+  })
+     }else{alert("Delete have been cancled")}
+
+
+
+  }
   
 
   return (
@@ -97,7 +133,7 @@ const filteredPersons = persons.filter(person =>
       
 
        <h3>Numbers</h3>
-       <Persons filteredPersons={ filteredPersons }/>
+       <Persons filteredPersons={ filteredPersons }  toggleTrash={toggleTrashOf}/>
       
         
 
