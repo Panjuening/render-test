da39cf53d537b202b94e6666ada09469093ed6f3
panjuening <juening.pan@outlook.com>
2026-05-07 20:29:27 +0800

    5月7日后端第一次测试

--- render_test/part3exercise/index.cjs
+++ render_test/part3exercise/index.cjs
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const Person = require('./models/person')
 const morgan = require('morgan')
-const { default: person } = require('./models/person')
+const { default: person } = require('./models/person.jsx')
 const app = express()
 
 
